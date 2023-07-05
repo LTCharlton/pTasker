447dfc042306e62e9c04d274ff85fcb8b7ed4ad0
Lindsey Charlton <linser22@gmail.com>
2023-07-05 14:32:02 -0700

    The script is added
    It’s function all only return 0 at the moment

---
+++ pTasker.py
@@ -0,0 +1,25 @@
+#------------------
+import sys
+import csv
+
+#------------------global variables 
+user_input = sys.argv
+task = 0
+
+#-------------------functions
+
+def readInput():
+    return 0
+
+def addTask():
+    #this will save a task to a csv file within the folder
+    return 0
+
+def showTasks():
+    return 0
+
+def removeTask(task):
+    return 0
+
+#-------------------main
+print(showTasks())
